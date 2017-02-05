bdfe11698a7a29162d5e4663bb5c0d51837e9159
Nicolas Guillemot <nlguillemot@gmail.com>
2017-02-05 02:44:35 -0800

    hook up decoder to robdd

--- main.cpp
+++ main.cpp
@@ -8,53 +8,53 @@
 #include <map>
 #include <vector>
 #include <tuple>
+#include <cassert>
 
 struct node
 {
-    enum {
-        terminal_value_bit = 0b0001,
-        is_terminal_bit    = 0b0010,
-    };
-
     int var;
     const node* lo;
     const node* hi;
 };
 
-const node* const false_node = (node*)(0);
-const node* const true_node = (node*)(node::terminal_value_bit);
+node terminal_nodes[2] = {
+    node{ INT_MAX, &terminal_nodes[0], &terminal_nodes[0] },
+    node{ INT_MAX - 1, &terminal_nodes[1], &terminal_nodes[1] }
+};
+const node* const false_node = &terminal_nodes[0];
+const node* const true_node = &terminal_nodes[1];
 
 namespace opcode
 {
     enum {
-        disj = 0,
-        conj = 1
+        bdd_and,
+        bdd_or
     };
 }
 
 bool is_terminal(const node* n)
 {
-    return ((uintptr_t)n & node::is_terminal_bit) != 0;
+    return n == false_node || n == true_node;
 }
 
 bool terminal_value(const node* n)
 {
-    return ((uintptr_t)n & node::terminal_value_bit) != 0;
+    return n == true_node;
 }
 
 const node* operation(const node* bdd1, const node* bdd2, int op)
 {
     switch (op)
     {
-    case opcode::disj:
-        return (terminal_value(bdd1) || terminal_value(bdd2)) ? true_node : false_node;
-    case opcode::conj:
+    case opcode::bdd_and:
         return (terminal_value(bdd1) && terminal_value(bdd2)) ? true_node : false_node;
+    case opcode::bdd_or:
+        return (terminal_value(bdd1) || terminal_value(bdd2)) ? true_node : false_node;
     }
     return NULL;
 }
 
-class bdd_builder
+class robdd
 {
     struct node_cmp
     {
@@ -155,8 +155,14 @@ struct bdd_instr
     int operands[3];
 };
 
-void decode(bdd_instr* instrs, int num_instrs)
+void decode(
+    bdd_instr* instrs, int num_instrs,
+    int num_ast_nodes,
+    robdd* r)
 {
+    std::vector<const node*> ast2bdd_v(num_ast_nodes);
+    const node** ast2bdd = ast2bdd_v.data() - 1;
+
     for (int i = 0; i < num_instrs; i++)
     {
         const bdd_instr& inst = instrs[i];
@@ -164,27 +170,49 @@ void decode(bdd_instr* instrs, int num_instrs)
         switch (inst.opcode)
         {
         case bdd_instr::opcode_newvar:
-            printf("new %d\n", 
-                inst.operands[bdd_instr::operand_newvar_id]);
+        {
+            int ast_id = inst.operands[bdd_instr::operand_newvar_id];
+            printf("new %d\n", ast_id);
+
+            int var_id = ast_id; // straightforward mapping.
+            ast2bdd[ast_id] = r->make_node(ast_id, false_node, true_node);
+
             break;
+        }
         case bdd_instr::opcode_and:
-            printf("%d = %d AND %d\n", 
-                inst.operands[bdd_instr::operand_and_dst_id],
-                inst.operands[bdd_instr::operand_and_src1_id],
-                inst.operands[bdd_instr::operand_and_src2_id]);
+        {
+            int dst_ast_id = inst.operands[bdd_instr::operand_and_dst_id];
+            int src1_ast_id = inst.operands[bdd_instr::operand_and_src1_id];
+            int src2_ast_id = inst.operands[bdd_instr::operand_and_src2_id];
+            printf("%d = %d AND %d\n", dst_ast_id, src1_ast_id, src2_ast_id);
+
+            const node* src1_bdd = ast2bdd[src1_ast_id];
+            const node* src2_bdd = ast2bdd[src2_ast_id];
+            ast2bdd[dst_ast_id] = r->apply(src1_bdd, src2_bdd, opcode::bdd_and);
+
             break;
+        }
         case bdd_instr::opcode_or:
-            printf("%d = %d OR %d\n",
-                inst.operands[bdd_instr::operand_or_dst_id],
-                inst.operands[bdd_instr::operand_or_src1_id],
-                inst.operands[bdd_instr::operand_or_src2_id]);
+        {
+            int dst_ast_id = inst.operands[bdd_instr::operand_or_dst_id];
+            int src1_ast_id = inst.operands[bdd_instr::operand_or_src1_id];
+            int src2_ast_id = inst.operands[bdd_instr::operand_or_src2_id];
+            printf("%d = %d OR %d\n", dst_ast_id, src1_ast_id, src2_ast_id);
+
+            const node* src1_bdd = ast2bdd[src1_ast_id];
+            const node* src2_bdd = ast2bdd[src2_ast_id];
+            ast2bdd[dst_ast_id] = r->apply(src1_bdd, src2_bdd, opcode::bdd_or);
+
             break;
+        }
+        default:
+            assert(false);
         }
     }
 }
 
 std::vector<bdd_instr> g_bdd_instructions;
-int g_next_bdd_id = 1;
+int g_next_ast_id = 1;
 
 int l_var_newindex(lua_State* L)
 {
@@ -194,16 +222,16 @@ int l_var_newindex(lua_State* L)
 
 int l_var_index(lua_State* L)
 {
-    int* bdd_id = (int*)lua_newuserdata(L, sizeof(int));
-    *bdd_id = g_next_bdd_id;
-    g_next_bdd_id += 1;
+    int* ast_id = (int*)lua_newuserdata(L, sizeof(int));
+    *ast_id = g_next_ast_id;
+    g_next_ast_id += 1;
 
     bdd_instr new_instr;
     new_instr.opcode = bdd_instr::opcode_newvar;
-    new_instr.operands[bdd_instr::operand_newvar_id] = *bdd_id;
+    new_instr.operands[bdd_instr::operand_newvar_id] = *ast_id;
     g_bdd_instructions.push_back(new_instr);
 
-    luaL_newmetatable(L, "bdd");
+    luaL_newmetatable(L, "ast");
     lua_setmetatable(L, -2);
 
     // var[bdd_userdata] = var_name;
@@ -216,21 +244,21 @@ int l_var_index(lua_State* L)
 
 int l_and(lua_State* L)
 {
-    int* bdd1_id = (int*)luaL_checkudata(L, 1, "bdd");
-    int* bdd2_id = (int*)luaL_checkudata(L, 2, "bdd");
+    int* ast1_id = (int*)luaL_checkudata(L, 1, "ast");
+    int* ast2_id = (int*)luaL_checkudata(L, 2, "ast");
 
-    int* bdd_id = (int*)lua_newuserdata(L, sizeof(int));
-    *bdd_id = g_next_bdd_id;
-    g_next_bdd_id += 1;
+    int* ast_id = (int*)lua_newuserdata(L, sizeof(int));
+    *ast_id = g_next_ast_id;
+    g_next_ast_id += 1;
 
     bdd_instr and_instr;
     and_instr.opcode = bdd_instr::opcode_and;
-    and_instr.operands[bdd_instr::operand_and_dst_id] = *bdd_id;
-    and_instr.operands[bdd_instr::operand_and_src1_id] = *bdd1_id;
-    and_instr.operands[bdd_instr::operand_and_src2_id] = *bdd2_id;
+    and_instr.operands[bdd_instr::operand_and_dst_id] = *ast_id;
+    and_instr.operands[bdd_instr::operand_and_src1_id] = *ast1_id;
+    and_instr.operands[bdd_instr::operand_and_src2_id] = *ast2_id;
     g_bdd_instructions.push_back(and_instr);
 
-    luaL_newmetatable(L, "bdd");
+    luaL_newmetatable(L, "ast");
     lua_setmetatable(L, -2);
 
     return 1;
@@ -238,21 +266,21 @@ int l_and(lua_State* L)
 
 int l_or(lua_State* L)
 {
-    int* bdd1_id = (int*)luaL_checkudata(L, 1, "bdd");
-    int* bdd2_id = (int*)luaL_checkudata(L, 2, "bdd");
+    int* ast1_id = (int*)luaL_checkudata(L, 1, "ast");
+    int* ast2_id = (int*)luaL_checkudata(L, 2, "ast");
 
-    int* bdd_id = (int*)lua_newuserdata(L, sizeof(int));
-    *bdd_id = g_next_bdd_id;
-    g_next_bdd_id += 1;
+    int* ast_id = (int*)lua_newuserdata(L, sizeof(int));
+    *ast_id = g_next_ast_id;
+    g_next_ast_id += 1;
 
     bdd_instr or_instr;
     or_instr.opcode = bdd_instr::opcode_or;
-    or_instr.operands[bdd_instr::operand_or_dst_id] = *bdd_id;
-    or_instr.operands[bdd_instr::operand_or_src1_id] = *bdd1_id;
-    or_instr.operands[bdd_instr::operand_or_src2_id] = *bdd2_id;
+    or_instr.operands[bdd_instr::operand_or_dst_id] = *ast_id;
+    or_instr.operands[bdd_instr::operand_or_src1_id] = *ast1_id;
+    or_instr.operands[bdd_instr::operand_or_src2_id] = *ast2_id;
     g_bdd_instructions.push_back(or_instr);
 
-    luaL_newmetatable(L, "bdd");
+    luaL_newmetatable(L, "ast");
     lua_setmetatable(L, -2);
 
     return 1;
@@ -269,7 +297,7 @@ int main(int argc, char* argv[])
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
 
-    luaL_newmetatable(L, "bdd");
+    luaL_newmetatable(L, "ast");
     {
         lua_pushcfunction(L, l_and);
         lua_setfield(L, -2, "__mul");
@@ -300,5 +328,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    decode(g_bdd_instructions.data(), (int)g_bdd_instructions.size());
+    int num_ast_nodes = g_next_ast_id - 1;
+    robdd r;
+    decode(g_bdd_instructions.data(), (int)g_bdd_instructions.size(), num_ast_nodes, &r);
 }
